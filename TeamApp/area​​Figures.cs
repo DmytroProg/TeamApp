b5a1510ecb9142f686bdca904c2782f9b19dbbdb
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-06-12 20:42:34 +0300

    Add comments

--- TeamApp/area​​Figures.cs
+++ TeamApp/area​​Figures.cs
@@ -2,6 +2,14 @@
 {
     public class Area​​Figures
     {
+
+        /// <summary>
+        /// Calculates the area of a triangle given its base length and height.
+        /// </summary>
+        /// <param name="baseLength"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         static public double AreaTriangle(double baseLength, double height)
         {
             if (baseLength < 0 || height < 0)
@@ -12,6 +20,12 @@
         }
 
 
+        /// <summary>
+        /// Calculates the area of a circle given its radius.
+        /// </summary>
+        /// <param name="radius">The radius of the circle. Must be non-negative.</param>
+        /// <returns>The area of the circle as a <see cref="double"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="radius"/> is negative.</exception>
         static public double AreaCircle(double radius)
         {
             if (radius < 0)
@@ -21,7 +35,12 @@
             return Math.PI * radius * radius;
         }
 
-
+        /// <summary>
+        /// Calculates the area of a square given the length of its side.
+        /// </summary>
+        /// <param name="sideLength">The length of one side of the square. Must be non-negative.</param>
+        /// <returns>The area of the square, calculated as the square of <paramref name="sideLength"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="sideLength"/> is negative.</exception>
         static public double AreaSquare(double sideLength)
         {
             if (sideLength < 0)
