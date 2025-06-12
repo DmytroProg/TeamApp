66acc49f34be3af5a2329faa5796d5d2e2e7675c
Mazur-Stas <mazur.stas5400@gmail.com>
2025-06-12 20:33:46 +0300

    add new class

---
+++ TeamApp/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamApp
+{
+    public class AreaCalculator
+    {   
+        public double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double CalculateSquareArea(double side)
+        {
+            return side * side;
+        }
+
+        public double CalculateTriangleArea(double Base, double height)
+        {
+            return 0.5 * Base * height;
+        }
+    }
+}
