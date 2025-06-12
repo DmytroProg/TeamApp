b569368f30117aa73393a42daee95058cf8a220f
oket23 <oleggolub460@gmail.com>
2025-06-12 20:48:30 +0300

    fixed v2

--- TeamApp/Program.cs
+++ TeamApp/Program.cs
@@ -1,31 +1,18 @@
-﻿using TeamApp;
+﻿Console.WriteLine("New commit");
 
+Console.WriteLine("Hello world!");
+Console.WriteLine("New commit");
+Console.WriteLine("123");
+static void ShowMenu()
+{
+    Console.Write("1.Get all hero\n2.Get hero by id\n3.Get all episode\n4.Get all episode order by name\n5.Exit\nSelect option: ");
+}
+
+Console.WriteLine("New text");
 
 while (true)
 {
     ShowMenu();
     var option = Console.ReadLine();
 
-    switch (option)
-    {
-        default:
-            Console.WriteLine("\nSelect valid option!\n");
-            break;
-        case "1":
-            MathService.GetCircleArea();
-            break;
-        case "2":
-            MathService.GetSquareArea();
-            break;
-        case "3":
-            MathService.GetTriangleArea();
-            break;
-        case "4":
-            Console.WriteLine("\nBye-bye...");
-            return;
-    }
-}
-static void ShowMenu()
-{
-    Console.Write("1.Get Circle Area\n2.Get Square Area\n3.Get Triangle Area\n4.Exit\nSelect option: ");
 }
