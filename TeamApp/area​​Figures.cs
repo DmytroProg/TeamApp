323a47eb8364cc20c846aca878ca02082d6fcdc5
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-06-12 20:31:43 +0300

    Add Class AreaFigure

---
+++ TeamApp/area​​Figures.cs
@@ -0,0 +1,21 @@
+﻿namespace TeamApp
+{
+    public class Area​​Figures
+    {
+        public double AreaTriangle(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+
+
+        public double AreaCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double AreaSquare(double sideLength)
+        {
+            return sideLength * sideLength;
+        }
+    }
+}
