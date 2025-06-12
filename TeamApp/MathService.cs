19ed483e2dd33952dd7d739a82fc5ce5d0ad6510
oket23 <oleggolub460@gmail.com>
2025-06-12 20:40:16 +0300

    ready!

---
+++ TeamApp/MathService.cs
@@ -0,0 +1,17 @@
+﻿namespace TeamApp;
+
+public class MathService
+{
+    public double GetCircleArea(double r)
+    {
+        return Math.PI * r * r;
+    }
+    public double GetSquareArea(double a)
+    {
+        return a * a;
+    }
+    public double GetTriangleArea(double a,double h)
+    {
+        return 0.5 * a * h;
+    }
+}
--- TeamApp/Program.cs
+++ TeamApp/Program.cs
@@ -1,18 +1,56 @@
-﻿Console.WriteLine("New commit");
+﻿
+using TeamApp;
 
-Console.WriteLine("Hello world!");
-Console.WriteLine("New commit");
-Console.WriteLine("123");
-static void ShowMenu()
-{
-    Console.Write("1.Get all hero\n2.Get hero by id\n3.Get all episode\n4.Get all episode order by name\n5.Exit\nSelect option: ");
-}
-
-Console.WriteLine("New text");
+var service = new MathService();
 
 while (true)
 {
     ShowMenu();
     var option = Console.ReadLine();
 
+    switch (option)
+    {
+        default:
+            Console.WriteLine("\nSelect valid option!\n");
+            break;
+        case "1":
+            Console.Write("\nEnter circle radius: ");
+            if (!double.TryParse(Console.ReadLine(), out double radius))
+            {
+                Console.WriteLine("\nEnter valid radius!\n");
+            }
+            var circleArea = service.GetCircleArea(radius);
+            Console.WriteLine($"\nCircle area: {circleArea}\n");
+            break;
+        case "2":
+            Console.Write("\nEnter square side: ");
+            if (!double.TryParse(Console.ReadLine(), out double side))
+            {
+                Console.WriteLine("\nEnter valid side!\n");
+            }
+            var squareArea = service.GetSquareArea(side);
+            Console.WriteLine($"\nSquare area: {squareArea}\n");
+            break;
+        case "3":
+            Console.Write("\nEnter the base of the triangle: ");
+            if (!double.TryParse(Console.ReadLine(), out double triangleBase))
+            {
+                Console.WriteLine("\nEnter valid base!\n");
+            }
+            Console.Write("\nEnter the height of the triangle: ");
+            if (!double.TryParse(Console.ReadLine(), out double triangleHeight))
+            {
+                Console.WriteLine("\nEnter valid height!\n");
+            }
+            var triangleArea = service.GetTriangleArea(triangleBase, triangleHeight);
+            Console.WriteLine($"\nTriangle area: {triangleArea}\n");
+            break;
+        case "4":
+            Console.WriteLine("\nBye-bye...");
+            return;
+    }
+}
+static void ShowMenu()
+{
+    Console.Write("1.Get Circle Area\n2.Get Square Area\n3.Get Triangle Area\n4.Exit\nSelect option: ");
 }
